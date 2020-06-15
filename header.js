893f01dc7f7e203a5a694810317859ecfe424e9e
catalintgv <64461746+catalintgv@users.noreply.github.com>
2020-06-15 22:42:09 +0300

    Add files via upload

--- header.js
+++ header.js
@@ -1,8 +1,8 @@
 // adaugam header-ul in html
 mainPage.innerHTML += `
 	<div id="header">
-		<h1>Magazinul de telefoane La Baiatu'</h1>
-		<p>Pst, baiatu', nu vrei un telefon?</p>
+		<h1>Magazinul de telefoane </h1>
+		<p>Adauga telefoane in lista de mai jos!</p>
 	</div>
 `;
 
